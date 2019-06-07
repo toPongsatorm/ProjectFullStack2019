69ed728e9823a909917f8c867f785b11338b8c9c
BRoWn <15932020@aru.ac.th>
2019-06-07 13:38:03 +0700

    Database

--- smalllibrary/book/urls.py
+++ smalllibrary/book/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('', home, name='home'),
     path('auth_page/', auth_page, name='auth_page'),
     path('logout/', logoutView, name='logout'),
-    path('sent/',sent,name='sent'),
+    path('sent/<int:pk>',sent,name='sent'),
     path('list',list_item,name='list_item')
  
 ]
--- smalllibrary/book/views.py
+++ smalllibrary/book/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
 from .models import Book,Transaction
-from .form import ItemForm
+from .forms import ItemForm
 # Create your views here.
 
 
@@ -36,10 +36,10 @@ def logoutView(request):
     logout(request)
     return redirect('home')
 
-def sent(request,pks):
+def sent(request,pk):
         if request.method =='POST':
                 try:
-                    book = Book.objects.get(pk=pks)
+                    book = Book.objects.get(pk=pk)
                     status = int(request.POST['amount'])
                     book.status -1
                     book.save()
@@ -52,6 +52,10 @@ def sent(request,pks):
                         print(e)
                         return e
         else:
-                book =Book.objects.get(pk=pks)
-                context = {'book':book}
+                book =Book.objects.get(pk=pk)
+                form = ItemForm(instance=book)
+                context = {
+                        'book':book,
+                        'form': form
+                }
                 return render(request,'sent.html',context)
